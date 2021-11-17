7f30dc7eff5f977a4fee7afbdd446fe2f6516b26
kPOWz <karrie.zeman@gmail.com>
2021-11-16 22:50:20 -0600

    sets up tests for part 3

--- src/test/java/org/example/WordFrequencyStreamReaderTest.java
+++ src/test/java/org/example/WordFrequencyStreamReaderTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -32,7 +34,7 @@ class WordFrequencyStreamReaderTest {
     }
 
     @Test
-    void shouldGetAsManyAsPossible() {
+    void shouldGetAsManyWordsAsPossible() {
         String words = "dog cat bird cat dog donkey horse dog";
         var ssr = new WordFrequencyStreamReader(words, 12);
 
@@ -41,6 +43,17 @@ class WordFrequencyStreamReaderTest {
         String[] expectedTop2 = {"cat", "dog", "bird", "donkey", "horse"};
         assertTrue(expectedTop2.length == observed.length);
         assertTrue(Arrays.asList(expectedTop2).containsAll(Arrays.asList(observed)));
+    }
+
+    @Test
+    @Description("3) Exclude a set of common words (case insensitive) (the,of,to,and,a,in,is,it,you,that,he,was,for,on,are,with,as,I,his,they,be,at,one,have,this,from,or,had,by,not,word,but,what,some,we,can,out,other,were,all,there,when,up,use,your,how,said,an,each,she)")
+    void shouldDisqualifyWordsBasedOnPredefinedExclusionsList() {
+        assertTrue(false);
+    }
+
+    @Test
+    void shouldIgnoreCase() {
+        assertTrue(false);
     }
 
 }
